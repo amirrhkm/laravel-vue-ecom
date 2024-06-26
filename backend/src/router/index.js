f26a93aaa6d6bfa0c0345c85e2526030399ab800
amir.nurhakim <207092@student.upm.edu.my>
2024-06-26 17:10:26 +0800

    update router

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,14 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import dashboard from "../views/dashboard.vue";
 import login from "../views/login.vue";
+import requestPassword from "../views/request-password.vue";
+import resetPassword from "../views/reset-password.vue";
 
 const routes = [
-    {
-        path: 'dashboard', name: 'dashboard', component: dashboard
-    },
-    {
-        path: 'login', name: 'login', component: login
-    }
+    { path: '/dashboard', name: 'dashboard', component: dashboard },
+    { path: '/login', name: 'login', component: login },
+    { path: '/request-password', name: 'requestPassword', component: requestPassword} ,
+    { path: '/reset-password', name: 'resetPassword', component: resetPassword }
 ];
 
 const router = createRouter({
